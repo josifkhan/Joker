47ea02226d83fe7d9aaea04873f54a17a5216d76
Md Josif Khan <114861784+josifkhan@users.noreply.github.com>
2023-08-03 15:29:11 +0600

    Add files via upload

--- joker.py
+++ joker.py
@@ -1,1 +1,5 @@
-import test
+from platform import architecture as arch
+import sys
+if '64bit' in arch():import reg
+else:sys.exit('\033[38;5;208m [+] Your device is not supported for this tool.\033[38;5;188mContact admin (facebook.com/josifvai)\033[0m')
+	
