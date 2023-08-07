8b3c7be9284c83a5620f684ecb972e56546e437f
Md Josif Khan <114861784+josifkhan@users.noreply.github.com>
2023-08-07 11:24:52 +0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-import requests,os,sys,time,re,random
-import string,hashlib
-import platform
-from datetime import datetime
-import mysql.connector
-from bs4 import BeautifulSoup as bs
-from random import sample as sq
-from string import *
-
-
+import os
 os.system('pip install requests')
 os.system('pip install mysql-connector')
 os.system('pip install bs4')
