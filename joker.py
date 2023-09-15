60cbd37f67d10ed4c4896af0b15352898fb8eae3
Md Josif Khan <114861784+josifkhan@users.noreply.github.com>
2023-09-16 04:53:51 +0600

    Delete joker.py

--- joker.py
+++
@@ -1,5 +0,0 @@
-from platform import architecture as arch
-import sys
-if '64bit' in arch():import reg
-else:sys.exit('\033[38;5;208m [+] Your device is not supported for this tool.\033[38;5;188mContact admin (facebook.com/josifvai)\033[0m')
-	
